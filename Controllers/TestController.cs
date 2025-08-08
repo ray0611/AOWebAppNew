5b38816a8e9e8c04348ee2615355fe74c909e4b5
Ruohan Wang <rayyy020611@gmail.com>
2025-08-08 15:34:57 +0930

    week1

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AOWebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RazorTest()
+        {
+            return View();
+        }
+    }
+
+}
