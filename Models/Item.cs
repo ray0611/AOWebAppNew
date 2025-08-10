16bcdd2ebc5feda68c0b7ef76d45ca2e058682c6
Ruohan Wang <rayyy020611@gmail.com>
2025-08-10 18:08:40 +0930

    Scaffolding the Views, Adding Model Validation, Add the Item View to the Layout

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AOWebApp.Models;
 
@@ -7,21 +9,24 @@ public partial class Item
 {
     public int ItemId { get; set; }
 
+    [Required]
+    [StringLength(150)]
     public string ItemName { get; set; } = null!;
 
+    [Required]
     public string ItemDescription { get; set; } = null!;
 
+    [Required]
+    [Column(TypeName = "decimal(10, 2)")]
     public decimal ItemCost { get; set; }
 
+    [Required]
     public string ItemImage { get; set; } = null!;
 
-    public int CategoryId { get; set; }
+    public int? CategoryId { get; set; }
 
-    public virtual ItemCategory Category { get; set; } = null!;
-
+    public virtual ItemCategory? Category { get; set; }
     public virtual ICollection<ItemMarkupHistory> ItemMarkupHistories { get; set; } = new List<ItemMarkupHistory>();
-
     public virtual ICollection<ItemsInOrder> ItemsInOrders { get; set; } = new List<ItemsInOrder>();
-
     public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
 }
