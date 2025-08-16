da58b4134f07f90a004f8f4eb2e19b3ac8426a43
Ruohan Wang <rayyy020611@gmail.com>
2025-08-16 19:56:04 +0930

    Creating the ViewModel

---
+++ ViewModels/ItemSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AOWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AOWebApp.ViewModels
+{
+    public class ItemSearchViewModel
+    {
+        public string SearchText { get; set; }
+        public int? CategoryId { get; set; }
+        public SelectList? CategoryList { get; set; }
+        public List<Item> ItemList { get; set; } = new();
+
+    }
+}
