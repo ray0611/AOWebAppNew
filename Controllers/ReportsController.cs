183a034cdbb033bdafb8a15b59fc9271cd95e264
Ruohan Wang <rayyy020611@gmail.com>
2025-10-12 19:08:22 +1030

    Create ReportsController + Views

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,19 @@
+﻿using AOWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AOWebApp.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly AmazonOrdersDbContext _context;
+
+        public ReportsController(AmazonOrdersDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View("AnnualSalesReport");
+        }
+    }
+}
