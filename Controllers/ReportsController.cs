527eb19593e9dd38d0b61a34b5fb53da9ffc996f
Ruohan Wang <rayyy020611@gmail.com>
2025-10-12 19:52:23 +1030

    the Report Json

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -21,5 +21,31 @@ namespace AOWebApp.Controllers
                 .ToList();
             return View("AnnualSalesReport", new SelectList(yearList));
         }
+
+        public IActionResult AnnualSalesReportData(int Year)
+        {
+            if (Year > 0)
+            {
+                var orderSummary = _context.ItemsInOrders
+                    .Where(iio => iio.OrderNumberNavigation.OrderDate.Year == Year)
+                    .GroupBy(iio => new { iio.OrderNumberNavigation.OrderDate.Year, iio.OrderNumberNavigation.OrderDate.Month })
+                    .Select(group => new
+                    {
+                        year = group.Key.Year,
+                        monthNo = group.Key.Month,
+                        monthName = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(group.Key.Month),
+                        totalItems = group.Sum(iio => iio.NumberOf),
+                        totalSales = group.Sum(iio => iio.TotalItemCost)
+                    })
+                    .OrderBy(data => data.monthNo);
+
+                return Json(orderSummary);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
     }
 }
