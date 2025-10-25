b48fe1c89bb673f203feee9e2de768286fcafc68
Ruohan Wang <rayyy020611@gmail.com>
2025-10-25 12:27:35 +1030

    Installing React Router, Using Link Components

--- aowebapp/aowebapp/src/App.js
+++ aowebapp/aowebapp/src/App.js
@@ -1,44 +1,26 @@
-import './App.css';
-import Card from './components/Card'
-import CardV2 from './components/CardV2'
-import CardV3 from './components/CardV3'
-import CardList from './components/CardList'
-import CardListNew from './components/CardListNew'
-import CardListSearch from './components/CardListSearch'
-
+import { Link } from "react-router-dom";
+import CardList from './components/CardListSearch';
 
 function App() {
-    return (
-        <div className="App container">
-            <div className="bg-light py-1 mb-2">
-                <h2 className="text-center">Example Application</h2>
+return (
+    <div className="App container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">AOWebApp</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link className="nav-link active" to="/Home">Home</Link>
+                        <Link className="nav-link" to="/Contact">Contact</Link>
+                        <Link className="nav-link" to="/Products">Products</Link>
+                    </div>
+                </div>
             </div>
-            <div className="row justify-content-center">
-                <Card itemID="1"
-                    itemName="test record1"
-                    itemDescription="test record1 DESC"
-                    itemImage="https://upload.wikimedia.org/wikipedia/commons/0/04/So_happy_smiling_cat.jpg"
-                    itemCost='15.00'
-                />
-                <CardV2 itemID="2"
-                    itemName="test record2"
-                    itemDescription="test record2 DESC"
-                    itemImage="https://upload.wikimedia.org/wikipedia/commons/0/04/So_happy_smiling_cat.jpg"
-                    itemCost='10.00'
-                />
-                <CardV3 itemID="3"
-                    itemName="test record3"
-                    itemDescription="test record3 DESC"
-                    itemImage="https://upload.wikimedia.org/wikipedia/commons/0/04/So_happy_smiling_cat.jpg"
-                    itemCost='5.00'
-                />
-                <CardListSearch></CardListSearch>
-
-
-            </div>
-
-        </div>
-    );
+        </nav>
+    </div>
+);
 }
 
 export default App;
