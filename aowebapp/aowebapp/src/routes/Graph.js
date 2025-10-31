2519dc399c168e61706688bb6877fcfa0f3ad7a0
Ruohan Wang <rayyy020611@gmail.com>
2025-11-01 10:03:27 +1030

    Setting up a dynamic data source

--- aowebapp/aowebapp/src/routes/Graph.js
+++ aowebapp/aowebapp/src/routes/Graph.js
@@ -1,8 +1,32 @@
-﻿export default function Graph() {
+﻿import React, { useState, useEffect } from "react";
+export default function Graph() {
+    const [rngNumber, setRngNumber] = useState(0);
+    const [rngArray, setRngArray] = useState([]);
+    const maxItems = 20;
+    const timeOut = 500;
+    const maxValue = 60;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRngNumber(Math.floor(Math.random() * maxValue));
+        }, timeOut);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        let tempArray = [...rngArray, rngNumber];
+        if (tempArray.length > maxItems) {
+            tempArray.shift(); 
+        }
+        setRngArray(tempArray);
+        // console.log(rngArray);
+    }, [rngNumber]);
+
     return (
         <div className="App container">
             <h1>
-                RNG Output:
+                RNG Output: {rngNumber}
             </h1>
             <div className="row">
                 <svg
